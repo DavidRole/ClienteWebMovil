fd26d3b6301c54de009ff8a1982e9f3c6e6af29e
FelipeAV23 <luisfelipealfaro2324@gmail.com>
2025-07-09 01:43:05 -0600

    Update view and login initiation done
    Update view done, initiated the logic

--- src/app/pages/update/update.ts
+++ src/app/pages/update/update.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './update.css'
 })
 export class Update {
+  registerObj: any = {
+    firstName: '',
+    lastName:  '',
+    email:     '',
+    address:   '',
+    currentPassword:  '',
+    newPassword: '',
+  };
 
+  constructor() {}
+  
+  onUpdate() {
+    // Logic to handle the update operation
+    console.log('Update operation initiated with:', this.registerObj);
+    // Here you would typically call a service to perform the update
+  }
 }
